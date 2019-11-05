73977db1bbf3c8269f5fde1c84c67d5a823f4f1a
Alejo Carballude <alejocarballude@gmail.com>
2019-11-05 19:37:36 -0300

    Add Integration-1 Kubernetes config alias.

--- fish/kubectl_aliases.fish
+++ fish/kubectl_aliases.fish
@@ -1,5 +1,6 @@
 # kubectl specific configs
 alias ki='kubectl --kubeconfig=$HOME/.kube/config_integration'
+alias ki1='kubectl --kubeconfig=$HOME/.kube/config_integration-1'
 alias ks='kubectl --kubeconfig=$HOME/.kube/config_staging'
 alias kp='kubectl --kubeconfig=$HOME/.kube/config_production'
 
