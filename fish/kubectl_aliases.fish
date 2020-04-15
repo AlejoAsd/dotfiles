61ac63ff9d2c2771d79c969e81f45cca5f85484a
Alejo Carballude <alejocarballude@gmail.com>
2020-04-15 10:35:16 -0400

    Add kube-system Kubernetes aliases. Add git lg alias without --all.

--- fish/kubectl_aliases.fish
+++ fish/kubectl_aliases.fish
@@ -1,6 +1,13 @@
 # kubectl specific configs
+alias k='kubectl'
 alias ki='kubectl --kubeconfig=$HOME/.kube/config_integration'
+alias kin='kubectl --kubeconfig=$HOME/.kube/config_integration -n kube-system'
 alias ki1='kubectl --kubeconfig=$HOME/.kube/config_integration-1'
-alias ks='kubectl --kubeconfig=$HOME/.kube/config_staging'
-alias kp='kubectl --kubeconfig=$HOME/.kube/config_production'
+alias kt='kubectl --kubeconfig=$HOME/.kube/config_test'
 
+alias ki1n='kubectl --kubeconfig=$HOME/.kube/config_integration-1 -n kube-system'
+alias ks='kubectl --kubeconfig=$HOME/.kube/config_staging'
+alias ksn='kubectl --kubeconfig=$HOME/.kube/config_staging -n kube-system'
+alias kp='kubectl --kubeconfig=$HOME/.kube/config_production'
+alias kpn='kubectl --kubeconfig=$HOME/.kube/config_production -n kube-system'
+alias ktn='kubectl --kubeconfig=$HOME/.kube/config_test -n kube-system'
